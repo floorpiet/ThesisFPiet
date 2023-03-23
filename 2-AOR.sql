1081157ac6f1fec07183700f45633194a29e7e59
Floor Piet <113601549+floorpiet@users.noreply.github.com>
2023-03-23 14:26:36 +0100

    Update 2-AOR.sql

--- 2-AOR.sql
+++ 2-AOR.sql
@@ -1,61 +1,46 @@
 /*
-two price tables tables: one for ppl price and one for default price (default price table needs different join w.r.t. skeleton)
-there can be multiple prices for an article on one day so therefore use RN
+ Get the demand for a given order-date, article and price line
+ AOR calculated as nr articles sold in PPL on a date divided by total nr orders on that date in PPL.
+ Only regular sales used (excl. promo sales)
+ -> interpretation: probability that article is ordered (on a specified date and PPL)
+ */
 
-! PAY ATTENTION: 2 QUERIES TO RUN
-*/
-
--- need to update: ``If you are using ft_price as your source for purchasing prices, time to move to ft_article_purchasing_price - > ft_price will soon be deprecated.''
-
--- ppl price table
-CREATE OR REPLACE TABLE temp.fpiet_prices AS (
-                                             WITH
-                                               all_prices AS
-                                                 (
-                                               SELECT
-                                                 skeleton.*,
-                                                 IFF(skeleton.ppl = 'default', NULL, ftp.price_in_cents)                                                                AS price_in_cents_ppl,
-                                                 IFF(skeleton.ppl = 'default', ftp.price_in_cents, NULL)                                                                AS price_in_cents_default,
-                                                 -- Need to include row number rn
-                                                 -- This column is used to query last changed price on given date only
-                                                 ROW_NUMBER() OVER (PARTITION BY skeleton.key_article,skeleton.key_date,skeleton.ppl ORDER BY price_end_date_time DESC) AS rn
-                                               FROM temp.fpiet_skeleton AS skeleton
-                                                 left outer JOIN dim.ft_price AS ftp
-                                                            ON skeleton.key_article = ftp.key_article
-                                                              AND
-                                                               skeleton.date BETWEEN ftp.price_start_date_time::DATE AND ftp.price_end_date_time::DATE
-                                                              AND ftp.price_type = 'PicNic'
-                                                              AND skeleton.ppl = ftp.price_line_type
-                                                 )
-                                             SELECT *
-                                             FROM all_prices
-                                             WHERE rn = '1' -- used last set price on a day (in case >1 price changes on a day)
-                                             ORDER BY key_article, key_date, ppl
-                                             );
-
--- default price table
-CREATE OR REPLACE TABLE temp.fpiet_defprices AS (
-                                             WITH
-                                               all_prices AS
-                                                 (
-                                               SELECT
-                                                 skeleton.*,
-                                                 ftp.price_in_cents AS price_in_cents_default,
-                                                 -- Need to include row number rn
-                                                 -- This column is used to query last changed price on given date only
-                                                 ROW_NUMBER() OVER (PARTITION BY skeleton.key_article,skeleton.key_date,skeleton.ppl ORDER BY price_end_date_time DESC) AS rn
-                                               FROM temp.fpiet_skeleton AS skeleton
-                                                 left outer JOIN dim.ft_price AS ftp
-                                                            ON skeleton.key_article = ftp.key_article
-                                                              AND
-                                                               skeleton.date BETWEEN ftp.price_start_date_time::DATE AND ftp.price_end_date_time::DATE
-                                                              AND ftp.price_type = 'PicNic'
-                                                              and ftp.price_line_type = 'default'
-                                              order by key_article, key_date, ppl
-                                                 )
-                                             SELECT *
-                                             FROM all_prices
-                                             WHERE rn = '1' -- used last set price on a day (in case >1 price changes on a day)
-                                             ORDER BY key_article, key_date, ppl
-                                             );
-
+CREATE OR REPLACE TABLE temp.fpiet_aor AS (
+                                          WITH aorppl AS (--first table: for order info per PPL
+                                            SELECT -- count orders per PPL (for divisor AOR)
+                                                     -- ftol.key_article,
+                                                     ftol.key_order_date,
+                                                     do.order_price_line_type,
+                                                     COUNT(DISTINCT ftol.key_order)   AS nr_orders_day_ppl,
+                                                     SUM(ftol.orig_regular_sales_qty) AS sum_regular_sales,
+                                                     SUM(ftol.orig_promo_qty)         AS sum_promo_sales
+                                              FROM dim.ft_orderline AS ftol
+                                                INNER JOIN dm_order AS do --NOTE: was only valid until june 2022 (PPL shift - now fixed)
+                                                           ON ftol.key_order = do.key_order
+                                              WHERE ftol.key_order_date BETWEEN $start_date AND $end_date
+                                              GROUP BY ftol.key_order_date, do.order_price_line_type
+                                              ORDER BY ftol.key_order_date, do.order_price_line_type
+                                                         )
+                                          SELECT
+                                            ftol.key_article,
+                                            ftol.key_order_date,
+                                            do.order_price_line_type                                  AS ppl,
+                                            aorppl.nr_orders_day_ppl,
+                                            IFF(SUM(ftol.orig_regular_sales_qty) > 0, SUM(ftol.orig_regular_sales_qty),
+                                                0)                                                    AS sum_regular_art_sales_ppl,
+                                            --SUM(ftol.orig_promo_qty)                              AS sum_promo_art_sales_ppl, -- decided to disregard promo data
+                                            DIV0(sum_regular_art_sales_ppl, aorppl.nr_orders_day_ppl) AS aor
+                                          FROM ft_orderline AS ftol
+                                            INNER JOIN dm_order AS do
+                                                       ON ftol.key_order = do.key_order
+                                            RIGHT OUTER JOIN aorppl --temp.fpiet_aor_ppl AS aorppl (earlier version used temp table instead of WITH AS () -statement
+                                                             ON ftol.key_order_date = aorppl.key_order_date
+                                                               AND
+                                                                do.order_price_line_type = aorppl.order_price_line_type
+                                          GROUP BY ftol.key_article,
+                                                   ftol.key_order_date,
+                                                   do.order_price_line_type,
+                                                   aorppl.nr_orders_day_ppl
+                                          ORDER BY key_order_date, key_article, ppl
+                                          )
+;
